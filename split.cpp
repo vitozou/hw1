d7de148839ac1cb2c7ccf87a85108af6982f06ba
Vito Zou <vzou@usc.edu>
2024-01-26 05:36:16 +0000

    all done with part !

--- split.cpp
+++ split.cpp
@@ -11,13 +11,96 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
+void sort(Node*& set, Node* current);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  if(in == NULL || in == nullptr) {
+    return;
+  }
+  
+
+  int x = in->value;
+  Node* theNext = in->next;
+  
+  // odd
+  if(x % 2 == 1) {
+    // if odds is empty
+    if(odds == NULL || odds == nullptr) {
+      odds = in;
+      in->next = nullptr;
+    }
+    else { // odds != nullptr
+      sort(odds, in);
+      
+    }
+  }
+  else if(x % 2 == 0) { // even
+    // if evens is empty
+    if(evens == NULL || evens == nullptr) {
+      evens = in;
+      in->next = nullptr;
+      // delete in;
+    }
+    else {
+      // Node* temp = in;
+      sort(evens, in);
+    }
+  }
+  else {
+    // std::cerr<<"ERROR: why is x neither??"<<std::endl;
+    return;
+  }
+  in = NULL;
+  // std::cout<<"split once"<<std::endl;
+  split(theNext, odds, evens);
+  
+
 }
 
 /* If you needed a helper function, write it here */
+
+void sort(Node*& set, Node* current) {
+  
+  // set already has something inside of it
+  Node* theNext = set->next;
+
+  // for the case where set's size = 1 oor when we reach its end
+  if(set == nullptr) {
+    current->next = nullptr;
+    set = current;
+    return;
+  }
+  
+  // if set size = 1
+  if(set->next == nullptr) {
+    if(current->value < set->value) {
+      current->next = set;
+      set = current;
+      return;
+    }
+    else { // current is larger than the current set
+      set->next = current;
+      current->next = nullptr;
+      return;
+    }
+  }
+
+  if(current->value >= (set->next)->value) {
+    sort(set->next, current);
+  }
+  else { // current->value < set->value
+    // replace 
+    current->next = theNext;
+    set->next = current;
+    return;
+  }
+  
+ return;
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,53 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
 
+    Node* h = new Node(4, nullptr);
+    Node* g = new Node(12, h);
+    Node* f = new Node(2, g);
+    Node* e = new Node(8, f);
+    
+    Node* d = new Node(3, e);
+    Node* c = new Node(5, d);
+    Node* b = new Node(9, c);
+    Node* a = new Node(1, b);
+
+    // Node* a = new Node(2, nullptr);
+    Node* nullNode = NULL;
+
+    
+
+    // while(a != nullptr) {
+    //     cout<<a->value<<endl;
+    //     a = a->next;
+    // }
+    Node* odds = NULL;
+    Node* evens = NULL;
+
+    cout<<"Hello!"<<endl;
+    split(a, odds, evens);
+    // split(nullNode, odds, evens);
+    // sort(odds, a);
+    
+    // cout<<"Is odds empty: "<<(odds == nullptr)<<endl;
+
+    while(odds != nullptr) {
+        cout<<"For odds, "<<odds->value<<endl;
+        odds = odds->next;
+    }
+
+    while(evens != nullptr) {
+        cout<<"For evens, "<<evens->value<<endl;
+        evens = evens->next;
+    }
+    
+    // currently odds includes only the first and last element
+
+    return 0;
 }
